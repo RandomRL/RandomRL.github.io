8598fa3526b9970a360778878827e7f8cfe1b4dc
mashw <37547251+mashw@users.noreply.github.com>
2019-06-16 20:20:25 +0100

    Added New Mutators and Changed Random Team Method
    Random team method adjusted to give better spread of results and added new "Curveball" mutator to ball physics.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*PORTRAIT ONLY MODE (FOR MOBILES*/
+/* PORTRAIT ONLY MODE (FOR MOBILES */
 window.onresize = function () {
   var mobile = false
   ;(function mobilecheck () {
@@ -10,7 +10,9 @@ window.onresize = function () {
         /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
           a.substr(0, 4)
         )
-      ) { mobile = true }
+      ) {
+        mobile = true
+      }
     })(navigator.userAgent || navigator.vendor || window.opera)
   })()
 
@@ -29,15 +31,16 @@ window.onresize = function () {
   }
 }
 
-/**RANDOM BACKGROUND CODE**/
+/** RANDOM BACKGROUND CODE**/
 var bgCount = 16
-var num = Math.ceil(Math.random() * bgCount);
-document.getElementById("bg").style.background = "url('images/bg/" + num + ".jpg') no-repeat center center fixed";
-document.getElementById("bg").style.backgroundSize = "cover";
+var num = Math.ceil(Math.random() * bgCount)
+document.getElementById('bg').style.background =
+  "url('images/bg/" + num + ".jpg') no-repeat center center fixed"
+document.getElementById('bg').style.backgroundSize = 'cover'
 
 /*******************************************************************/
 /** ARRAYS**/
-var players = ['Player 1', 'Player 2', 'Player 3', 'Player 4']
+var players = ['Si', 'Ross', 'Mash', 'Stace']
 var modes = [
   'Soccar',
   'Soccar',
@@ -128,7 +131,7 @@ var overTimeM = [
   '+5 Max, First Score',
   '+5 Max, Random Team'
 ]
-var seriesM = ['1', '1', '1', '1', '1', '1', '1', '3', '5', '7']
+var seriesM = ['1', '1', '1', '1', '1', '1', '1', '3', '3', '3', '5', '7']
 var gameSpeedM = [
   'Default',
   'Default',
@@ -152,7 +155,8 @@ var ballWeightM = [
   'Light',
   'Heavy',
   'Super Light',
-  '10th Anniversary'
+  '10th Anniversary',
+  'Curveball'
 ]
 var ballSizeM = ['Default', 'Default', 'Small', 'Large', 'Gigantic']
 var ballBouncinessM = ['Default', 'Default', 'Low', 'High', 'Super High']
@@ -227,9 +231,9 @@ quickPick.onclick = function quickPick () {
   }
 
   /** Map Selector**/
-  var map = ''
-
-  ;(function mapChoice () {
+  var map = '';
+  
+  (function mapChoice () {
     if (modeResult == 'Hoops') {
       map = 'Dunk House'
     } else if (modeResult == 'Dropshot') {
@@ -240,7 +244,6 @@ quickPick.onclick = function quickPick () {
   })()
 
   /** Select Background Image**/
-  var mapURL
   var mapImg = function (map) {
     var stripped = map.replace(/\W/g, '').toLowerCase()
     console.log(stripped + '.png')
@@ -305,7 +308,7 @@ quickPick.onclick = function quickPick () {
         ballMaxSpeed +
         '</li><li>Ball Type: ' +
         ballType() +
-        '</li><li>Ball Weight: ' +
+        '</li><li>Ball Physics: ' +
         ballWeight +
         '</li><li>Ball Size: ' +
         ballSize +
@@ -344,37 +347,29 @@ quickPick.onclick = function quickPick () {
   }, 700)
 
   /** ODOMETER**/
-  var points = function () {
-    var min = 100
-    var max = 400
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
 
-  var points2 = function() {    
-    min = 100;
-    mid1 = 500;
-    mid2 = 650;
-    max = 1000;
-    var regLimit = 1;
-    var midLimit = 0.10;
-    var maxLimit = 0.05;  
-    var num  = 0;
-    var r = Math.random();
+  var points2 = function () {
+    min = 100
+    mid1 = 500
+    mid2 = 650
+    max = 1000
+    var regLimit = 1
+    var midLimit = 0.1
+    var maxLimit = 0.05
+    var num = 0
+    var r = Math.random()
     if (r <= maxLimit) {
-      num = getRandomNum(mid2, max);
+      num = getRandomNum(mid2, max)
+    } else if (r > maxLimit && r <= midLimit) {
+      num = getRandomNum(mid1, mid2)
+    } else {
+      num = getRandomNum(min, mid1)
     }
-  
-    else if (r > maxLimit && r <= midLimit) {
-      num = getRandomNum(mid1, mid2);
-    }
-    else {
-      num = getRandomNum(min, mid1);
-    }  
-    return num; 
+    return num
   }
 
-  function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  function getRandomNum (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
   var odometer = document.getElementById('numberSpan')
